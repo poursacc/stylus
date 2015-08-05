0838e7f0ac1f096f71da31bee81ceb944206e81a
Clément Poursac <poursacc@yahoo.fr>
2015-08-05 19:48:42 +0200

    Ajout animation JS

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,30 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+	$(window).on("scroll", function() {
+		if($(this).scrollTop() > 20){ 
+			$(".navbar").css("background-color", "black");
+		}
+		else
+		{
+			$(".navbar").css("background-color", "transparent");
+
+		}	
+	});
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+});
